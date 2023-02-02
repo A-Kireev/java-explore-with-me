f05eb6861e7ff166e4cb498783f9a9e024f48571
Alexander <a.kireev.qa@gmail.com>
2023-02-02 19:26:59 +0300

    fix: убрать неиспользуемые импорты

--- stats-service/stats-server/src/main/java/ru/practicum/explorewithme/repository/StatsRepositoryCustom.java
+++ stats-service/stats-server/src/main/java/ru/practicum/explorewithme/repository/StatsRepositoryCustom.java
@@ -1,10 +1,7 @@
 package ru.practicum.explorewithme.repository;
 
 import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.practicum.explorewithme.HitStatDto;
-import ru.practicum.explorewithme.model.StatsEntity;
 
 public interface StatsRepositoryCustom {
 
