34662f218200474d75315a02d82d87b4a5d6c74d
Alexander <a.kireev.qa@gmail.com>
2023-02-14 00:07:07 +0300

    fix: вынести общий путь в RequestMapping

--- ewm-service/src/main/java/ru/practicum/explorewithme/request/RequestController.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/request/RequestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,33 +18,34 @@ import ru.practicum.explorewithme.request.dto.RequestStatusesDto;
 import ru.practicum.explorewithme.request.service.RequestService;
 
 @RestController
+@RequestMapping(path = "/users/{userId}")
 @RequiredArgsConstructor
 public class RequestController {
 
   private final RequestService requestService;
 
-  @GetMapping("/users/{userId}/requests")
+  @GetMapping("/requests")
   public List<RequestDto> getRequests(@PathVariable long userId) {
     return requestService.getRequests(userId);
   }
 
-  @PostMapping("/users/{userId}/requests")
+  @PostMapping("/requests")
   @ResponseStatus(HttpStatus.CREATED)
   public RequestDto createRequest(@PathVariable long userId, @RequestParam long eventId) {
     return requestService.createRequest(userId, eventId);
   }
 
-  @PatchMapping("/users/{userId}/requests/{requestId}/cancel")
+  @PatchMapping("/requests/{requestId}/cancel")
   public RequestDto cancelRequest(@PathVariable long userId, @PathVariable long requestId) {
     return requestService.cancelRequest(userId, requestId);
   }
 
-  @GetMapping("/users/{userId}/events/{eventId}/requests")
+  @GetMapping("/events/{eventId}/requests")
   public List<RequestDto> getEventRequests(@PathVariable long userId, @PathVariable long eventId) {
     return requestService.getEventRequests(userId, eventId);
   }
 
-  @PatchMapping("/users/{userId}/events/{eventId}/requests")
+  @PatchMapping("/events/{eventId}/requests")
   public RequestStatusesDto changeEventRequests(@PathVariable long userId,
       @RequestBody ChangeRequestStatusDto changeRequestStatusDto) {
     return requestService.changeEventRequests(userId, changeRequestStatusDto);
